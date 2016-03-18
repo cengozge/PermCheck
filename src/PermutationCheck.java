fe18dbc68918954be465951eb58f78428ef7f6fe
ozge.gokay <gokayozge@gmail.com>
2016-03-18 11:02:25 +0200

    first version

---
+++ src/PermutationCheck.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import conversion.Conversions;
+
+
+public class PermutationCheck {
+
+	public static void main(String[] args){
+		int[] A = {8,6,4};
+		//solution(A);
+	}
+	
+	public int solution(int[] A){
+		
+		List<Integer> list = Conversions.fromArrayToList(A);
+		Collections.sort(list);
+		
+		int tempVar = 0;
+		
+		for (Integer integer : list) {
+			if(++tempVar == integer){
+				;
+			}
+			else
+				return 0;
+		}
+		return 1;
+	}
+}
